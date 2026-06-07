f34d80a224de28ec03e11257277cb1ccc4fd0822
lancast <angelprep92@gmail.com>
2026-06-07 16:50:27 -0600

    refactor(pages): integrate shared PageBanner in Home and Transparencia

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { PageBanner } from '../../shared/page-banner/page-banner';
 
 @Component({
   selector: 'migala-home',
+  imports: [PageBanner],
   templateUrl: './home.html'
 })
 export class Home {}
--- src/app/pages/transparencia/transparencia.ts
+++ src/app/pages/transparencia/transparencia.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { PageBanner } from '../../shared/page-banner/page-banner';
 
 @Component({
   selector: 'migala-transparencia',
+  imports: [PageBanner],
   templateUrl: './transparencia.html'
 })
 export class Transparencia {}
